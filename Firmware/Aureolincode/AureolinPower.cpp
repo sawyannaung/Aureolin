857cc40ad9495454e5a60da20d1718699896d7f5
sawyannaung <sawyannaung547@gmail.com>
2020-02-08 16:41:09 -0500

    Firmware uploard

---
+++ Firmware/Aureolincode/AureolinPower.cpp
@@ -0,0 +1,5 @@
+bool AureolinPower::isCharging()
+{
+  !digitalRead(pin);  //'!' takes the inverse
+  uint16_t AureolinPower::getBatteryVoltage()
+}
---
+++ Firmware/Aureolincode/AureolinPower.h
@@ -0,0 +1,21 @@
+#ifndef AureolinPower_h
+#define AureolinPower_h
+
+//Standard Arduino libraries
+
+#include <Arduino.h>
+
+class AureolinPower
+
+{
+  private:
+
+
+  public:
+
+    bool isCharging()
+    uint16_t getBatteryVoltage();
+    
+};
+
+#endif
